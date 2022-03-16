491b06ac6cc0e00c6afb25c02c57b159eef56b07
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2022-03-16 20:59:30 +0700

    new

---
+++ component/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import React from 'react'
+import NavBar from './NavBar';
+
+const Layout: React.FC = ({children}) => {
+    return (
+        <div>
+            <Head>
+            <meta charSet="UTF-8"></meta>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+            <title>Selling Book</title>
+            </Head>
+            <header>
+                <NavBar></NavBar>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <h1>footer</h1>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import React from 'react'
+import Layout from '../component/Layout'
+
+const Home: React.FC = () => {
+  return (
+    <Layout>
+        <h2>body</h2>
+    </Layout>
+  )
+}
+
+
+export default Home
+
