7ea2493e2acf423dd9a6027ea7a73d2c65e1bfb3
Lương Thiện Phước <19522055@gm.uit.edu.vn>
2022-04-18 23:29:56 +0700

    new

--- component/Admin/NavigationMobile.tsx
+++ component/Admin/NavigationMobile.tsx
@@ -10,12 +10,12 @@ function NavigationMobile(props) {
 
     return (
         <div className='min:block 900px:hidden'>
-            <div className='900px:hidden top-2/3 left-0 sm:block absolute hover:cursor-pointer' onClick={() => { isOpen(true) }}>
+            <div className='900px:hidden top-1/2 left-0 sm:block fixed hover:cursor-pointer' onClick={() => { isOpen(true) }}>
                 <BiDotsVerticalRounded></BiDotsVerticalRounded>
             </div>
             {
                 open ? (
-                    <div onClick={(e) => { isOpen(false) }} className='fixed bg-[#0000006e] top-0 left-0 right-0 bottom-0'>
+                    <div onClick={(e) => { isOpen(false) }} style={{zIndex: '100'}} className='fixed bg-[#0000006e] top-0 left-0 right-0 bottom-0 '>
                         <div onClick={(e) => { e.stopPropagation() }} className='bg-white left-0 h-full w-[300px] relative'>
                             {/* <div onClick={() => { isOpen(false) }} className='absolute -right-3 top-1/2 text-2xl z-50'>
                                 <BsFillArrowLeftCircleFill ></BsFillArrowLeftCircleFill>
--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import LinearProgress from '@mui/material/LinearProgress';
 import Head from 'next/head';
 import { Container, Grid } from '@mui/material';
-
+import { GrDocumentPdf } from 'react-icons/gr'
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 const Navigation = dynamic(() => import('../../component/Admin/Navigation'))
 const NavigationMobile = dynamic(() => import('../../component/Admin/NavigationMobile'))
 const Layout = dynamic(() => import('../../component/Layout'),
@@ -11,14 +12,19 @@ const Layout = dynamic(() => import('../../component/Layout'),
         loading: () => <LinearProgress></LinearProgress>
     }
 )
+const GeneralStatistics = dynamic(() => import('../../component/Admin/Dashboard/GeneralStatistics'))
+const Traffic = dynamic(() => import('../../component/Admin/Dashboard/Traffic'),
+    {
+        ssr: false
+    }
+)
+const UserChart = dynamic(() => import('../../component/Admin/Dashboard/UserChart'),
+    {
+        ssr: false
+    }
+)
 
 function dashboard(props) {
-    const [option, setOption] = useState('Danh sách đơn hàng')
-    const handleClickOption = (e) => {
-        e.preventDefault()
-        setOption(e.target.innerText)
-    }
-
     return (
         <Layout>
             <Head>
@@ -30,9 +36,19 @@ function dashboard(props) {
                     <Grid className='mt-16 min:hidden 900px:block' item sm={0} md={3}>
                         <Navigation option='dashboard'></Navigation>
                     </Grid>
-                    <Grid className='mt-16' item sm={12} md={9}>
-                        <div style={{ boxShadow: 'rgb(0 0 0 / 60%) 0px 3px 8px', padding: '25px' }} className='rounded-lg '>
-                            Dashboard
+                    <Grid className='mt-16 ' item sm={12} md={9}>
+                        <div style={{ boxShadow: 'rgb(0 0 0 / 60%) 0px 3px 8px' }} className='rounded-lg pb-3'>
+                            <GeneralStatistics></GeneralStatistics>
+                            <Traffic></Traffic>
+                            <UserChart></UserChart>
+                            <Traffic></Traffic>
+                            <Traffic></Traffic>
+                            <div className='flex justify-end mr-5 mt-3'>
+                                <div className='flex bg-[#2BBCBA] shadow-sm rounded text-lg items-center py-1 px-2 text-white cursor-pointer'>
+                                    <PictureAsPdfIcon className='mr-2'></PictureAsPdfIcon>
+                                    <div>XUẤT PDF</div>
+                                </div>
+                            </div>
                         </div>
                     </Grid>
                 </Grid>
